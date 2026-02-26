c46600094f05d09cb8f8ffa2868cfcc4b0d611f8
FranMoraCz17 <francisco.mora.cabezas@est.una.ac.cr>
2026-02-25 19:38:48 -0600

    estructura base limpia sin tabs

---
+++ src/screens/userProfile/UserProfile.style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const userStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#641919",
+  },
+});
---
+++ src/screens/userProfile/UserProfile.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+import ScreenWrapper from "@/src/components/ScreenWrapper/ScreenWrapper";
+export default function UserProfile() {
+  return (
+    <ScreenWrapper>
+      <Text>Pantalla de usuarios</Text>
+    </ScreenWrapper>
+  );
+}
